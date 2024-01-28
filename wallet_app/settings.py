6cd544f91db9cdeaec9255a48dac4410a2b05f8c
ivanmarinoff <marinoffivan@gmail.com>
2024-01-28 15:09:22 +0200

    Updated staticfiles

--- wallet_app/settings.py
+++ wallet_app/settings.py
@@ -113,7 +113,7 @@ STORAGES = {
         "BACKEND": "django.core.files.storage.FileSystemStorage",
     },
     "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+        "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
     },
 }
 
